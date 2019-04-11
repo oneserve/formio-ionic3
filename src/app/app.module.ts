f423e3269c8d539d6cbdd07622d1b3e4b33d0134
Bartosz Szpinda <bartosz.szpinda@oneserve.co.uk>
2019-04-11 08:35:19 +0200

    add angular-formio module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
+import { FormioModule } from 'angular-formio';
 
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
@@ -21,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    FormioModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
